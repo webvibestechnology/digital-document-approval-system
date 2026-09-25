695bc6e49ae3bc8f7eb19a17da3f27cf1c2b9c62
divya gujar <gujardivya982@gmail.com>
2026-09-25 18:03:17 +0530

    added new component

---
+++ Backend/documentapproval/src/main/java/com/documentapproval/HelloController.java
@@ -0,0 +1,13 @@
+package com.documentapproval;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>My Spring Boot project has started successfully!</h1>";
+    }
+}
